b83b07d4603c5f8851e13ff0886a119150eb9694
JesseEmond <emond.jesse@gmail.com>
2016-04-09 13:22:19 -0400

    Move fees sum in coinbase value calculation

--- miner/coinbase.py
+++ miner/coinbase.py
@@ -1,8 +1,9 @@
 from bitcoin import (START_REWARD, REWARD_MAX_HALVINGS,
+
                      REWARD_HALVING_INTERVAL)
 
 
-def calculate_coinbase_value(height, fees):
+def calculate_coinbase_value(height, transactions):
     """
     Calculates the amount of satoshis that can be spent from a coinbase
     transaction.
@@ -10,11 +11,13 @@ def calculate_coinbase_value(height, fees):
     This depends on the amount of satoshis mined (based on current height)
     and transaction fees included in the mined block.
 
-    :param height: current height of the blockchain
-    :param fees:   sum of the transaction fees in the block
-    :returns:      amount of satoshis produced by the coinbase transaction
+    :param height:         current height of the blockchain
+    :param transactions:   transactions included in the mined block
+    :returns:              amount of satoshis produced by the coinbase
+                           transaction
     """
     halvings = height // REWARD_HALVING_INTERVAL
+    fees = sum(tx.fees for tx in transactions)
 
     if halvings >= REWARD_MAX_HALVINGS:
         return fees
--- miner/coinbase_test.py
+++ miner/coinbase_test.py
@@ -4,58 +4,62 @@ from coinbase import calculate_coinbase_value
 
 
 class CoinbaseTest(unittest.TestCase):
+    class TransactionMock:
+        def __init__(self):
+            self.fees = 100000
+
     def test_coinbase_value_genesis_no_fees(self):
         height = 0
-        fees = 0
+        txs = []
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(5000000000, value)
 
     def test_coinbase_value_genesis_fees(self):
         height = 0
-        fees = 100000
+        txs = [CoinbaseTest.TransactionMock()]
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(5000100000, value)
 
     def test_coinbase_value_no_halving_no_fees(self):
         height = 209999
-        fees = 0
+        txs = []
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(5000000000, value)
 
     def test_coinbase_value_no_halving_fees(self):
         height = 209999
-        fees = 100000
+        txs = [CoinbaseTest.TransactionMock()]
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(5000100000, value)
 
     def test_coinbase_value_first_halving_no_fees(self):
         height = 210000
-        fees = 0
+        txs = []
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(2500000000, value)
 
     def test_coinbase_value_first_halving_fees(self):
         height = 210000
-        fees = 100000
+        txs = [CoinbaseTest.TransactionMock()]
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(2500100000, value)
 
     def test_coinbase_value_max_halving_no_fees(self):
         height = 2**64
-        fees = 0
+        txs = []
 
-        value = calculate_coinbase_value(height, fees)
+        value = calculate_coinbase_value(height, txs)
 
         self.assertEqual(0, value)
