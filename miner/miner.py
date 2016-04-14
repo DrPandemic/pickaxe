f3164ff052f9bc607ff3726df3fb2177b3eb6247
JesseEmond <emond.jesse@gmail.com>
2016-04-14 14:45:39 -0400

    Add missing package to server & condvar
    Condition variable in the miner.
    Missing zmq in server's package.json.
    Removed unused require "credentials.json".

--- miner/miner.py
+++ miner/miner.py
@@ -1,12 +1,17 @@
 #!/bin/python
+
 import zmq
-from threading import Thread
-from time import sleep
+from threading import Thread, Condition
+
+from protocol import compose_mining_result, parse_mining_task
+
 
 context = zmq.Context()
 HOST = "tcp://127.0.0.1:"
 PORT = 8080
 template = None
+template_received = Condition()
+
 
 def listening_thread():
     global template
@@ -16,19 +21,25 @@ def listening_thread():
     sock.connect(HOST + str(PORT))
     while True:
         template = sock.recv_json()
+        with template_received:
+            template_received.notify()
+
 
 def submit_block(block):
     with context.socket(zmq.PUSH) as sock:
         sock.connect(HOST + str(PORT + 1))
         sock.send_string(block)
 
+
 thread = Thread(target=listening_thread)
 thread.start()
 
 while True:
     if template:
-        print("Girl look at that body, I work out")
+        print("Working on template %s" % template["hash"])
         submit_block("my awesome block")
-        sleep(1)
+
     else:
-        sleep(1)
+        print("Waiting for work from pool leader.")
+        with template_received:
+            template_received.wait_for(lambda: template is not None)
