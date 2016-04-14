a288e9014665e400c4027932e23167eb6c7539c4
JesseEmond <emond.jesse@gmail.com>
2016-04-13 20:16:53 -0400

    Public Key Hash Extraction From Address (#14)
    * Add base58 requirement and address test
    * Address extraction implementation
    * Coverage specify source folder

---
+++ miner/address.py
@@ -0,0 +1,19 @@
+from base58 import b58decode_check
+
+
+def p2pkh_address_to_pubkey_hash(address):
+    """
+    Takes a P2PKH address (starting with a 1, m or n symbol) and extracts its
+    HASH160 hash (used as a public key hash).
+
+    :see: https://en.bitcoin.it/wiki/List_of_address_prefixes
+
+    :param address: P2PKH public address
+    :returns:       HASH160 hash of the public key
+    """
+    decoded = b58decode_check(address)
+
+    # check that it is a mainnet or testnet P2PKH address
+    assert(decoded[0] in [0x00, 0x6F])
+
+    return decoded[1:]  # skip version byte
