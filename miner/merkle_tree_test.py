e968fb20a65152da9fcccd7d4adb38f403d514b5
JesseEmond <emond.jesse@gmail.com>
2016-04-15 08:08:51 -0400

    Fix Merkle Tree Byte Ordering & Integration Test (#17)
    * Coinbase value calculation
    
    * Move fees sum in coinbase value calculation
    
    * Fix block reward terminology
    
    * Add P2PKH script generation & test
    
    * Serialize coinbase transaction & test
    
    * Remove block reward calculation
    
    Reason: it will be provided by the pool leader.
    
    * Add integration test file
    
    * Start working on mining task class
    
    * Add base58 requirement and address test
    
    * Address extraction implementation
    
    * Trailing comma
    
    * [minor] Remove trailing comma in comment
    
    * Coverage specify source folder
    
    * Mining task initial implementation
    
    * Add mining task test file
    
    * MiningTask implementation and test
    
    Also change transaction to hash its data (and test it).
    
    * Add protocol function to parse/produce JSON + test
    
    * Add more checks in protocol parsing test
    
    * Integration test
    
    * Fix merkle tree byte ordering bug

--- miner/merkle_tree_test.py
+++ miner/merkle_tree_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from merkle_tree import MerkleTree
+from helpers import to_rpc_byte_order
 
 
 class TestMerkleTree(unittest.TestCase):
@@ -11,22 +12,22 @@ class TestMerkleTree(unittest.TestCase):
     def test_root_even_transactions(self):
         # based on the block #125552 in the mainchain
         transactions = [
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "51d37bdd871c9e1f4d5541be67a6ab62"
                 "5e32028744d7d4609d0c37747b40cd2d"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "60c25dda8d41f8d3d7d5c6249e2ea1b0"
                 "5a25bf7ae2ad6d904b512b31f997e1a1"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "01f314cdd8566d3e5dbdd97de2d9fbfb"
                 "fd6873e916a00d48758282cbb81a45b9"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "b519286a1040da6ad83c783eb2872659"
                 "eaf57b1bec088e614776ffe7dc8f6d01"
-            ))
+            )))
         ]
 
         tree = MerkleTree(transactions)
@@ -40,34 +41,34 @@ class TestMerkleTree(unittest.TestCase):
     def test_root_odd_transactions(self):
         # based on the block #125553 in the mainchain
         transactions = [
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "3cfc035221a3d8eb8cdef98330467dea"
                 "51ee8f75cf0cfa2fcc1bb1e150191e57"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "446e1c006bbc0d61f7fe4f6a325d468d"
                 "6dd6016ace9b611370c9854e57aab0ac"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "d59ad81b484be54d97f693cfe1a5f450"
                 "1948ffce13f4a558e6feb3713a1eeff6"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "6d1e61f43ec0eba4804ad74aaeff1e13"
                 "7bef9bdf57098c352e6e8aeb27b95c6a"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "068b30fb5b6989bfbe0c0d5e5bca5dd9"
                 "f9eb100dda21fbac0b16fed436da8f0b"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "fd48d02e10d6629d385f642879b0dbe6"
                 "a12551fd011797f82dc80d56216bfcc4"
-            )),
-            TestMerkleTree.TransactionMock(bytes.fromhex(
+            ))),
+            TestMerkleTree.TransactionMock(to_rpc_byte_order(bytes.fromhex(
                 "2ec24502228833d687e9036a047f9d33"
                 "880f34b237a6703c864de55a6df1013a"
-            ))
+            )))
         ]
 
         tree = MerkleTree(transactions)
